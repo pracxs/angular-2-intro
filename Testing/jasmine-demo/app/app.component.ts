9d50f00fcd868d2822dcc06e79ff0506fa3bff9a
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2016-12-02 04:44:04 +0200

    Angular 2.2 Testing

--- Testing/jasmine-demo/app/app.component.ts
+++ Testing/jasmine-demo/app/app.component.ts
@@ -1,13 +1,11 @@
 import {Component} from '@angular/core';
-import {MyUpperPipe} from './my-upper.pipe'
 
 @Component({
     selector: 'my-app',
-    template: `
+    template: ` Name:
         {{firstName | myUpper}}
-    `,
-    pipes: [MyUpperPipe]
+    `
 })
 export class AppComponent {
     firstName = 'Todor'
- }
+}
--- Testing/jasmine-demo/app/my-upper.pipe.ts
+++ Testing/jasmine-demo/app/my-upper.pipe.ts
@@ -3,7 +3,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'myUpper'})
 export class MyUpperPipe implements PipeTransform {
   transform(value: string): string {
-    // return value && value.toUpperCase();
-    return null;
+    return value && value.toUpperCase();
+    // return null;
   }
 }
