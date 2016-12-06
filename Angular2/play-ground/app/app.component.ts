868e6dbe0abfc10a366ead927e768c900ab895e3
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2016-12-06 09:48:57 -0500

    List

--- Angular2/play-ground/app/app.component.ts
+++ Angular2/play-ground/app/app.component.ts
@@ -7,12 +7,32 @@
  * or to prometheus@itce.com
  */
 
-import {Component} from '@angular/core';
+import { Component } from '@angular/core'
+import { Contact } from './contact.interface'
 
 @Component({
     selector: 'my-app',
     template: `
-        Hello World!
+        <ul>
+            <li [class.active]="selected==contact" *ngFor="let contact of contacts" class='item'>
+                <a href='#' (click)="select(contact)">{{contact.firstName}} {{contact.lastName.toUpperCase()}}</a>
+                <a href='#' class='remove' title='Remove'><span class='glyphicon glyphicon-remove-sign'></span></a>
+            </li>
+        </ul>
     `
 })
-export class AppComponent {}
+export class AppComponent {
+    contacts = [
+        { id: "1", firstName: "Max", lastName: "Smith", email: "max@gmail.com" },
+        { id: "2", firstName: "Chris", lastName: "Raches", email: "chris@gmail.com" },
+        { id: "3", firstName: "Michael", lastName: "Alloy", email: "michael@gmail.com" },
+        { id: "4", firstName: "John", lastName: "Doe", email: "john@gmail.com" },
+        { id: "5", firstName: "Jenny", lastName: "Doe", email: "jenny@gmail.com" }
+    ]
+
+    selected: Contact
+
+    select(contact: Contact): void {
+        this.selected = contact
+    }
+}
