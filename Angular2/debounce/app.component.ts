8e0d6fe3df7385d7e84cde898427e0555794a4ff
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2016-12-07 19:36:00 -0500

    Refactored debounce example

--- Angular2/debounce/app.component.ts
+++ Angular2/debounce/app.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core'
-import { FormControl } from '@angular/forms';
+import { Component, AfterContentChecked, OnDestroy, ViewChild } from '@angular/core'
+import { FormControl, NgForm } from '@angular/forms'
+import { Subscription } from 'rxjs/Subscription'
 import 'rxjs/add/operator/debounceTime'
 
 @Component({
     selector: 'my-app',
     template: `
-        <input type=text [value]="firstName" [formControl]="firstNameControl"><br/>
-        {{firstName}}
+        <form #form="ngForm">
+            <input type=text name="value" [ngModel]="value"><br/>
+            Typed: {{value}}
+        </form>
     `
 })
-export class AppComponent implements OnInit {
-    firstName = ''
-    firstNameControl: FormControl
-    
-    constructor() { 
-        this.firstNameControl = new FormControl();
+export class AppComponent implements AfterContentChecked, OnDestroy {
+    value = ''
+    sub: Subscription
+
+    @ViewChild('form') form: NgForm
+
+    ngAfterContentChecked() {
+        if( ! this.form.controls['value'] ) // not inited yet
+            return
+
+        if( this.sub )
+            return
+
+        this.form.controls['value'].valueChanges
+             .debounceTime(1000)
+             .subscribe(newValue => this.value = <string> newValue)
     }
-    
-    ngOnInit() {
-        this.firstNameControl.valueChanges
-            .debounceTime(1000)
-            .subscribe(newValue => this.firstName = <string> newValue)
+
+    ngOnDestroy() {
+        this.sub.unsubscribe()
     }
  }
--- Angular2/debounce/app.module.ts
+++ Angular2/debounce/app.module.ts
@@ -7,13 +7,13 @@
  * or to prometheus@itce.com
  */
 
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent }  from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }   from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, ReactiveFormsModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
