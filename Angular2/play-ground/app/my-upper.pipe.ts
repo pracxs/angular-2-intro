ca8bf4d84fb4b64c5bd5581464069e3e78a3a87c
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2016-12-12 17:03:40 +0200

    1st custom Pipe

---
+++ Angular2/play-ground/app/my-upper.pipe.ts
@@ -0,0 +1,8 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'myUpper'})
+export class MyUpperPipe implements PipeTransform {
+  transform(value: string): string {
+    return value && ('' + value).toUpperCase();
+  }
+}
