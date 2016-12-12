a67dfe1ea1530593e64e1515e7a6614683fe2ebb
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2016-12-12 16:46:16 +0200

    List of contacts

--- Angular2/play-ground/app/app.component.ts
+++ Angular2/play-ground/app/app.component.ts
@@ -11,8 +11,26 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
+    styles: ['.hide { display: none !important; }'],
     template: `
-        Hello World!
+        <ul>
+            <li [class.active]="contact == selected" *ngFor="let contact of contacts" class='item'> 
+                <a href='#' (click)="selected = contact">{{contact.firstName}} {{contact.lastName.toUpperCase()}}</a>
+                <a href='#' onclick='ctrl.remove(event, " + contact.id + ")' class='remove' title='Remove'><span class='glyphicon glyphicon-remove-sign'></span></a>
+            </li>
+		</ul>
+
+        {{ selected?.id }}
     `
 })
-export class AppComponent {}
+export class AppComponent {
+    contacts = [
+				{ id: "1", firstName: "Max", lastName: "Smith", email: "max@gmail.com" },
+				{ id: "2", firstName: "Chris", lastName: "Raches", email: "chris@gmail.com" },
+				{ id: "3", firstName: "Michael", lastName: "Alloy", email: "michael@gmail.com" },
+				{ id: "4", firstName: "John", lastName: "Doe", email: "john@gmail.com" },
+				{ id: "5", firstName: "Jenny", lastName: "Doe", email: "jenny@gmail.com" }
+			]
+
+    selected: any
+}
