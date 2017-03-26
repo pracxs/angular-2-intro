054217d016c32dbc557bd490c9cddeb15e043a3a
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-03-26 18:22:08 +0300

    Sofia training start

--- Angular2/play-ground/app/app.component.ts
+++ Angular2/play-ground/app/app.component.ts
@@ -11,18 +11,8 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'my-app',
-    styles: [
-        "ul { margin-left: 10px; background: #eee; padding: 15px; }", 
-        "li {display: inline-block;}",
-        "li~li:before { content: '|'; margin: 0 7px 0 5px; }",
-        ".active, .active a { color: #c40030; }"],
     template: `
-        <ul>
-            <li [routerLinkActive]="['active']"><a [routerLink]="['contacts']">Contacts</a></li>
-            <li><a routerLink="about" [routerLinkActive]="['active']">About</a></li>
-        </ul>
-
-        <router-outlet></router-outlet>
+        <h1>Hello World!</h1>
     `
 })
 export class AppComponent {}
--- Angular2/play-ground/app/app.module.ts
+++ Angular2/play-ground/app/app.module.ts
@@ -10,12 +10,10 @@
 import { NgModule }      from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent }  from './app.component'
-import { AboutComponent } from './about/about.component'
-import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule ],
-  declarations: [ AppComponent, AboutComponent ],
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule {}
