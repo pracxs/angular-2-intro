93969a682eb622737c063530f7121e31e1035ff2
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-03-29 10:32:59 +0300

    Show contacts list

--- Angular2/play-ground/app/app.component.ts
+++ Angular2/play-ground/app/app.component.ts
@@ -8,11 +8,27 @@
  */
 
 import { Component } from '@angular/core'
+import { Contact } from './contact'
+
+const CONTACTS: Contact[] = [
+    { id: 1, firstName: "Max", lastName: "Smith", email: "max@gmail.com" },
+    { id: 2, firstName: "Chris", lastName: "Raches", email: "chris@gmail.com" },
+    { id: 3, firstName: "Michael", lastName: "Alloy", email: "michael@gmail.com" },
+    { id: 4, firstName: "John", lastName: "Doe", email: "john@gmail.com" },
+    { id: 5, firstName: "Jenny", lastName: "Doe", email: "jenny@gmail.com" }
+];
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Hello World!</h1>
+        <ul> 
+            <li class='item' *ngFor="let contact of contacts">
+                <a href='#' onclick='ctrl.select(event, contact.id)'>{{contact.firstName}} {{contact.lastName.toUpperCase()}}</a>
+                <a href='#' onclick='ctrl.remove(event, contact.id)' class='remove' title='Remove'><span class='glyphicon glyphicon-remove-sign'></span></a>
+            </li>
+		</ul>
     `
 })
-export class AppComponent {}
+export class AppComponent {
+    contacts: Contact[] = CONTACTS
+}
