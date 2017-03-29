9d5ba7aa99f54c74032d907232a400a9f094d639
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-03-29 12:57:35 +0300

    Edit details draft

--- Angular2/play-ground/app/contact-details.component.ts
+++ Angular2/play-ground/app/contact-details.component.ts
@@ -14,14 +14,35 @@ import { Contact } from './contact'
     selector: 'contact-details',
     template: `
         <div id="contactsDetailsContainer" *ngIf="contact">
-            <label>First Name: </label><b>{{contact.firstName}}</b><br/>
-            <label>Last Name: </label><b>{{contact.lastName}}</b><br/>
-            <label>email: </label><b>{{contact.email}}</b><br/>
-            <label></label><a href="#" class="text-danger" onclick="ctrl.edit(event,' contact.id)"><span class="glyphicon glyphicon-edit"></span>Edit</a><br/>
+            <span *ngIf="!showEdit">
+                <label>First Name: </label><b>{{contact.firstName}}</b><br/>
+                <label>Last Name: </label><b>{{contact.lastName}}</b><br/>
+                <label>email: </label><b>{{contact.email}}</b><br/>
+                <label></label><a href="#" class="text-danger" (click)="showEdit = true"><span class="glyphicon glyphicon-edit"></span>Edit</a><br/>
+            </span>
+            <form name="editContactForm" *ngIf="showEdit">
+                <label for="firstName">First Name: </label>
+                <input id="firstName" name="firstName" [value]="contact.firstName" required><br/>
+                
+                <label for="lastName">Last Name: </label>
+                <input id="lastName" name="lastName" [value]="contact.lastName" required><br/>
+                
+                <label for="email">email: </label>
+                <input id="email" name="email" [value]="contact.email" email><br/>
+                
+                <label></label>
+                <input type="submit" class="btn btn-danger" value="Save" />
+                <a href="#" class="text-danger" (click)="onCancel()">Cancel</a>
+            </form>
         </div>
     `
 })
 export class ContactDetailsComponent {
     @Input()
     contact: Contact
+    showEdit: boolean = false
+
+    onCancel() {
+        this.showEdit = false
+    }
 }
