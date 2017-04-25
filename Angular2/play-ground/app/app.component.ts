852f20fb9785413c513c8574fdbf900fc714eaac
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-04-25 04:39:48 -0600

    Init Angular 2 playground

--- Angular2/play-ground/app/app.component.ts
+++ Angular2/play-ground/app/app.component.ts
@@ -8,22 +8,11 @@
  */
 
 import { Component } from '@angular/core'
-import { DialogService } from "./dialog.service"
 
 @Component({
     selector: 'my-app',
-    providers: [DialogService],
-    styles: [
-        "ul { margin-left: 10px; background: #eee; padding: 15px; }", 
-        "li {display: inline-block;}",
-        "li~li:before { content: '|'; margin: 0 7px 0 5px; }",
-        ".active, .active a { color: #c40030; }"],
     template: `
-        <ul>
-            <li [routerLinkActive]="['active']"><a [routerLink]="['/contacts']">Contacts</a></li>
-            <li><a [routerLink]="['/about']" [routerLinkActive]="['active']">About</a></li>
-        </ul>
-        <router-outlet></router-outlet>
+        <h1>Hello World!</h1>
     `
 })
 export class AppComponent {}
--- Angular2/play-ground/app/app.module.ts
+++ Angular2/play-ground/app/app.module.ts
@@ -9,22 +9,11 @@
 
 import { NgModule }             from '@angular/core'
 import { BrowserModule }        from '@angular/platform-browser'
-import { FormsModule }          from '@angular/forms'
-import { RouterModule }         from '@angular/router'
 import { AppComponent }         from './app.component'
-import { ContactsComponent }    from './contacts/contacts.component'
-import { ContactsListComponent } from './contacts/contacts-list.component'
-import { ContactDetailsComponent } from './contacts/contact-details.component'
-import { EmailValidator }       from './email-validator.directive'
-import { ContactsService }      from './contacts/contact.service'
-import { AboutComponent }         from './about/about.component'
-
-import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, ContactsComponent, ContactsListComponent, ContactDetailsComponent, EmailValidator, AboutComponent ],
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
-  providers:    [ ContactsService ]
 })
 export class AppModule {}
