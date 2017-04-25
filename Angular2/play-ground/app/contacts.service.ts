c41bc8f621d3928e8eae73e6999eda40227f2a7f
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-04-25 16:51:42 -0600

    @Injectable()

--- Angular2/play-ground/app/contacts.service.ts
+++ Angular2/play-ground/app/contacts.service.ts
@@ -1,3 +1,5 @@
+import {Injectable} from "@angular/core"
+
 let _CONTACTS: Contact[] = [
             { id: 1, firstName: "Max", lastName: "Smith", email: "max@gmail.com" },
             { id: 2, firstName: "Chris", lastName: "Raches", email: "chris@gmail.com" },
@@ -6,6 +8,7 @@ let _CONTACTS: Contact[] = [
             { id: 5, firstName: "Jenny", lastName: "Doe", email: "jenny@gmail.com" }
         ];
 
+@Injectable()
 export class ContactsService {
     private CONTACTS = _CONTACTS
     private static _contactId = 5
