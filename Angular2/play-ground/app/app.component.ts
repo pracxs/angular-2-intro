17333bde1d46e60a71ca8cd8a602312a377ab56f
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-04-25 16:35:32 -0600

    *ngIf

--- Angular2/play-ground/app/app.component.ts
+++ Angular2/play-ground/app/app.component.ts
@@ -26,6 +26,13 @@ let _CONTACTS: Contact[] = [
                 <a href='#' onclick='ctrl.remove(event, " + contact.id + ")' class='remove' title='Remove'><span class='glyphicon glyphicon-remove-sign'></span></a>
             </li>
         </ul>
+
+        <div id="contactsDetailsContainer" *ngIf="selected">
+            <label>First Name: </label><b>{{selected?.firstName}}</b><br/>
+            <label>Last Name: </label><b>{{selected?.lastName}}</b><br/>
+            <label>email: </label><b>{{selected?.email}}</b><br/>
+            <label></label><a href="#" class="text-danger" onclick="ctrl.edit(event,' + contact.id + ')"><span class="glyphicon glyphicon-edit"></span>Edit</a><br/>
+        </div>
     `
 })
 export class AppComponent {
