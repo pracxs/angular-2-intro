712fd3ddaf6f2b34e35a10a31ae437478bedb873
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-04-27 16:14:18 -0600

    Preloading strategy

--- Angular2/play-ground/app/app-routing.module.ts
+++ Angular2/play-ground/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule }                 from '@angular/core'
-import { RouterModule, Routes }     from '@angular/router'
-import { FailComponent }            from "./fail/fail.component"
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router'
+import { FailComponent } from "./fail/fail.component"
+import { SelectivePreloadingStrategy } from "./selective-preloading-strategy";
 
 let routes: Routes = [
   { path: 'contacts',   loadChildren: 'app/contacts/contacts.module#ContactsModule' },
-  { path: 'about',      loadChildren: 'app/about/about.module#AboutModule' },
+  { path: 'about',      loadChildren: 'app/about/about.module#AboutModule', data: { preload: true } },
   { path: '',           redirectTo: '/contacts', pathMatch: 'full' },
   { path: '**',         component: FailComponent }
 ]
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
-  exports: [ RouterModule ]
+  imports: [ RouterModule.forRoot(
+                routes, 
+                { 
+                  useHash: true,
+                  preloadingStrategy: SelectivePreloadingStrategy  
+                }) 
+           ],
+  exports: [ RouterModule ],
+  providers: [ SelectivePreloadingStrategy ]
 })
 export class AppRoutingModule {}
