94677841dd05813af643285a80dc6ea4af273212
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-04-27 16:37:16 -0600

    Fix some discrepancies

--- Testing/play-ground/app/app.module.ts
+++ Testing/play-ground/app/app.module.ts
@@ -10,21 +10,13 @@
 import { NgModule }       from '@angular/core'
 import { BrowserModule }  from '@angular/platform-browser'
 import { HttpModule }     from '@angular/http';
-import { RouterModule, Route }     from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent }   from './app.component'
-import { PersonsDetailsComponent } from './details.component'
-import { GreeterComponent } from './greeter.component'
-import { AboutComponent }   from './about.component'
 
-let routes: Route[] = [
-  {path: '',       component: PersonsDetailsComponent, pathMatch: "full" },
-  {path: 'about',  component: AboutComponent},
-  {path: '**',     redirectTo: "/" }
-]
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule, RouterModule.forRoot(routes, {useHash: true}) ],
-  declarations: [ AppComponent, GreeterComponent, PersonsDetailsComponent, AboutComponent ],
+  imports:      [ BrowserModule, HttpModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule {}
