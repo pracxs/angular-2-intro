5854fb619b48e63f4e1dd276d21fccc27f542a12
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-04-27 10:06:20 -0600

    Fail component

---
+++ Angular2/play-ground/app/fail/fail.component.ts
@@ -0,0 +1,21 @@
+/* Copyright (C) 2016 Pracxs Net & ITCE - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the Prometheus courses license.
+ *
+ * You should have received a copy of the Prometheus courses
+ * license.If not, please write to: prometheus@pracxs.com
+ * or to prometheus@itce.com
+ */
+
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'contacts',
+    template: `
+        <h1>Upsss! Something went wrong!</h1>
+        <div class='about'>
+            We can't find the page you've typed
+        </div>
+    `
+})
+export class FailComponent {}
