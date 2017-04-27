f30cac55a7b272dffe39f335f5bdf4092bb05526
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-04-27 16:16:48 -0600

    Remove unneeded extra logging

--- Angular2/play-ground/app/selective-preloading-strategy.ts
+++ Angular2/play-ground/app/selective-preloading-strategy.ts
@@ -5,16 +5,8 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class SelectivePreloadingStrategy implements PreloadingStrategy {
-  preloadedModules: string[] = [];
-
   preload(route: Route, load: () => Observable<any>): Observable<any> {
     if (route.data && route.data['preload']) {
-      // add the route path to the preloaded module array
-      this.preloadedModules.push(route.path);
-
-      // log the route path to the console
-      console.log('Preloaded: ' + route.path);
-
       return load();
     } else {
       return Observable.of(null);
