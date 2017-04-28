2f0bcc51d6bf5338fafd2651fd58db3651c0bfc6
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2017-04-28 11:31:01 -0600

    Spy on dependant service

--- Testing/play-ground/app/person.service.get.spec.ts
+++ Testing/play-ground/app/person.service.get.spec.ts
@@ -5,15 +5,13 @@ import {PersonService} from './person.service'
 import {LoadPersonsService} from './load-persons.service'
 
 describe('PersonService', () => {
-    class LoadPersonsServiceMock {
-        load (): Person[] {
-            return [
-                { "id": 1, "firstName": "Max",     "lastName": "Smith",  "email": "max@gmail.com" },
-                { "id": 4, "firstName": "John",    "lastName": "Doe",    "email": "john@gmail.com" }
-            ]
-        }
-    }
+    let mock_contacts = [
+            { "id": 1, "firstName": "Max",     "lastName": "Smith",  "email": "max@gmail.com" },
+            { "id": 4, "firstName": "John",    "lastName": "Doe",    "email": "john@gmail.com" }
+        ]
     
+    let loadService: LoadPersonsService
+
     beforeAll( () => {
         TestBed.resetTestEnvironment()
         TestBed.initTestEnvironment( BrowserDynamicTestingModule, platformBrowserDynamicTesting() ) 
@@ -23,18 +21,21 @@ describe('PersonService', () => {
         TestBed.configureTestingModule({
             providers: [
                 PersonService,
-                { provide: LoadPersonsService, useClass: LoadPersonsServiceMock }
+                LoadPersonsService
             ]
         })
+
+        loadService = TestBed.get(LoadPersonsService)
+        spyOn(loadService, 'load').and.returnValue( mock_contacts )
     })
     
-    it('must have 2 persons', inject([PersonService], (personService) => {
-        let persons = personService.getAll();
+    it('must have 2 persons', inject([PersonService, LoadPersonsService], (personService, loadService) => {
+        let persons = personService.getAll()
         expect(persons.length).toBe(2)
     }))
     
     it('1st person id should 1', inject([PersonService], (personService) => {
         let persons = personService.getAll();
-        expect(persons[0].id).toBe(1)
+        expect(persons[1].id).toBe(4)
     }))
 })
